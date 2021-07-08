bf6296de0f3dc74101930f0cba4e9c13d3928376
joseph Farkas <redness123@protonmail.com>
2021-07-08 00:16:24 -0400

    Added userChoices array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
     public static void main(String[] args) {
+	ArrayList<Character> userChoices = new ArrayList<Character>();
 	System.out.println("Welcome to the Linux Quiz");
 	System.out.println("I will be guiding you through finding a linux distribution for your needs.");
-	QA.experience();
+	userChoices.add(QA.experience());
+
     }
 }
--- src/QA.java
+++ src/QA.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class QA {
-    public static void experience() {
+    public static char experience() {
         p.t("First question: How much linux experience do you have?");
         p.t("A: Just switching from Windows/Mac.");
         p.t("B: I've nibbled at it a few times, but nothing too deep");
@@ -9,6 +9,7 @@ public class QA {
         p.t("D: I consider myself a linux intermediate/expert but need to find another distro.");
         Scanner scanner = new Scanner(System.in);
         char answer = scanner.next().charAt(0);
+        return answer;
 
     }
 }
