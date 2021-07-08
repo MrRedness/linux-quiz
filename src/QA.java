d1411826564c6ed7ac1517efabdbfd8bd887ae5e
joseph Farkas <redness123@protonmail.com>
2021-07-08 00:24:56 -0400

    Added the packages class to QA

--- src/QA.java
+++ src/QA.java
@@ -8,8 +8,14 @@ public class QA {
         p.t("C: I've been using linux for a few weeks/months and I want to try a different distro.");
         p.t("D: I consider myself a linux intermediate/expert but need to find another distro.");
         Scanner scanner = new Scanner(System.in);
-        char answer = scanner.next().charAt(0);
-        return answer;
+        return scanner.next().charAt(0);
 
+    }
+    public static char packages() {
+        p.t("Ok. Next question: What type of package system do you prefer?");
+        p.t("(Packages are compiled software that can be installed on linux systems (the equivalent of a .exe on windows).");
+        p.t("Linux distros use 'package managers' to help install and keep up to date your software.");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next().charAt(0);
     }
 }
